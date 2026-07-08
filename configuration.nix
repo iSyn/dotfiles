67f1420f3d1a0d08f3c5b3c2548a16b974ee0842
synclairwang <synclairwang11@yahoo.com>
2026-07-07 23:46:58 -0400

    feat: initial setup stuff

---
+++ configuration.nix
@@ -0,0 +1,41 @@
+{ ... }:
+
+{
+  # Determinate already manages the Nix daemon, so nix-darwin shouldn't
+  nix.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.primaryUser = "synclairwang";
+  system.stateVersion = 6;
+
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      KeyRepeat = 2;              # fast key repeat
+      InitialKeyRepeat = 15;      # short delay before repeat
+      _HIHideMenuBar = true;      # auto-hide the menu bar
+      AppleShowAllExtensions = true;
+    };
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+    finder.CreateDesktop = false;          # clean desktop
+    trackpad.Clicking = true;              # tap to click
+  };
+
+  nix-homebrew = {
+    enable = true;
+    user = "synclairwang";
+    autoMigrate = true;  # take over the existing /opt/homebrew install, keeping packages
+  };
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";  # remove anything not listed here
+    onActivation.autoUpdate = true;
+    onActivation.extraFlags = [ "--force" ];
+    casks = [
+      "wezterm"
+    ];
+  };
+}
