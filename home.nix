9dac9d3dcc1a2a6e754e50d22af342de53d30760
synclairwang <synclairwang11@yahoo.com>
2026-07-08 00:06:02 -0400

    feat: added basic starship

--- home.nix
+++ home.nix
@@ -33,6 +33,19 @@ in
     };
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+      };
+      cmd_duration.format = "[$duration]($style) ";
+    };
+  };
+
   fonts.fontconfig.enable = true;
   home.sessionVariables.EDITOR = "nvim";
 
