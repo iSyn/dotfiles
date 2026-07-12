5b04c75d679d4600204ceb78c6aac927fb9ac4c5
synclairwang <synclairwang11@yahoo.com>
2026-07-12 12:13:44 -0400

    feat: added blink keymaps, show all imports

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,12 @@ vim.keymap.set("x", "p", [["_dP]], { desc = "Paste without yank" })
 -- half page scroll and recenter
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down + center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up + center" })
+
+-- copy project-relative file path (prefixed with @) to system clipboard
+vim.keymap.set("n", "<leader>cp", function()
+  local path = vim.fn.expand("%:p")
+  local rel = vim.fs.relpath(LazyVim.root(), path) or path
+  local mention = "@" .. rel
+  vim.fn.setreg("+", mention)
+  vim.notify("Copied: " .. mention)
+end, { desc = "Copy relative file path (@)" })
